9c307be78978ded3b7da2862055cf8f759b45974
shahrozenarsun <85216061+shahrozenarsun@users.noreply.github.com>
2021-12-02 19:21:40 +0500

    cha

---
+++ src/components/Chats.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import { AiOutlineCheck } from 'react-icons/ai';
+
+const Chats = (props) => {
+
+    console.log(props)
+
+    const [selected, setSelected] = useState(false);
+
+    useEffect(() => {
+        setSelected(props.selectAll)
+    }, [props])
+
+    return (
+        <div>
+            <div style={{ display: 'flex', flexDirection: 'row', width: '95%', borderBottom: '1px solid lightgray', position: 'relative', margin: '1% 0%' }}>
+                <Button
+                    onClick={() => setSelected(!selected)}
+                    style={{
+                        backgroundColor: selected ? "#09A391" : "#B3BFD2",
+                        borderRadius: '35px', height: "40px",
+                        minWidth: "10px", margin: 'auto'
+                    }} variant="contained"><AiOutlineCheck style={{ backgroundColor: "color" }}
+                    /></Button>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', lineHeight: '0' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', paddingLeft: '2%' }}>
+                        <img src="https://dummyimage.com/600x400/000/fff"
+                            style={{
+                                height: "55px",
+                                borderRadius: "30px",
+                                width: "55px",
+                                margin: 'auto'
+                            }}
+                        />
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <p style={{ fontWeight: 'bold' }}>Hamza</p>
+                            <p style={{ color: "#B6C1D4" }}>033000005522</p>
+                        </div>
+                    </div>
+                    <div style={{
+                        position: "absolute",
+                        right: "0",
+                        // bottom: "0",
+                        margin: 'auto'
+                    }}>
+                        <Button style={{
+                            backgroundColor: "#09A391", borderRadius: '35px', height: "40px",
+                            minWidth: "10px",
+                        }} variant="contained">+</Button>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Chats
---
+++ src/components/Main.js
@@ -0,0 +1,169 @@
+import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { CgMenuRight } from 'react-icons/cg';
+import { AiOutlineCheck } from 'react-icons/ai';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Chats from "./Chats";
+import makeStyles from '@mui/styles/makeStyles';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const useStyles = makeStyles({
+  pillOne: {
+    margin: 0,
+    cursor: 'pointer',
+    backgroundColor: "#F3F5F9",
+    padding: '1%',
+    '&:hover': {
+      backgroundColor: "#DCECEE",
+    },
+  },
+  pillTwo: {
+    margin: 0,
+    cursor: 'pointer',
+    padding: '1%',
+    '&:hover': {
+      backgroundColor: "#DCECEE",
+    },
+  },
+  message: {
+    width: '45%',
+    backgroundColor: '#F3F5F9',
+    color: '#DEE3ED',
+  }
+});
+
+const Main = () => {
+  const classes = useStyles();
+
+  const [selectAll, setSelectAll] = useState(false)
+
+  const selectAllContacts = () => {
+    setSelectAll(!selectAll)
+  }
+
+  return (
+    <div>
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={4} style={{ position: 'relative' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+              <div>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '90%' }}>
+                  <span style={{ fontSize: '30px', height: '30px', cursor: 'pointer', margin: "auto 0" }}><CgMenuRight style={{ transform: "rotateX(180deg)" }} /></span>
+                  <h2 style={{ marginLeft: '-40%' }}>Audience</h2>
+                  <h3 style={{ margin: "auto 0", color: "#F3F5F9" }}>100 Contacts</h3>
+                </div>
+                <div style={{ width: "90%", margin: 'auto' }}>
+                  <div style={{ padding: '0 2%' }}>
+                    <h3>Include Tags</h3>
+                    <div style={{ borderRadius: '15px' }}>
+                      <p className={classes.pillOne}>Greetings</p>
+                      <p className={classes.pillTwo}>Greetings</p>
+                      <p className={classes.pillOne}>Greetings</p>
+                      <p className={classes.pillTwo}>Greetings</p>
+                    </div>
+                  </div>
+                  <div style={{ padding: '0 2%' }}>
+                    <h3>Exclude Tags</h3>
+                    <div style={{ borderRadius: '15px' }}>
+                      <p className={classes.pillOne}>Greetings</p>
+                      <p className={classes.pillTwo}>Greetings</p>
+                      <p className={classes.pillOne}>Greetings</p>
+                      <p className={classes.pillTwo}>Greetings</p>
+                    </div>
+                  </div>
+                  <div style={{ padding: '0 2%' }}>
+                    <h3>Message Sent</h3>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <TextField placeholder="min" id="outlined-basic" variant="outlined" className={classes.message} />
+                      <TextField placeholder="max" id="outlined-basic" variant="outlined" className={classes.message} />
+                    </div>
+                  </div>
+                  <div style={{ padding: '0 2%' }}>
+                    <h3>Message Received</h3>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <TextField placeholder="min" id="outlined-basic" variant="outlined" className={classes.message} />
+                      <TextField placeholder="max" id="outlined-basic" variant="outlined" className={classes.message} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div style={{
+                position: "absolute",
+                bottom: "0",
+                width: "90%",
+              }}>
+                <Button style={{
+                  backgroundColor: "#09A391", borderRadius: '10px', height: "40px",
+                  minWidth: "90%", textTransform: 'capitalize', marginLeft: '9%'
+                }} variant="contained">Save Filters</Button>
+              </div>
+            </div>
+
+
+          </Grid>
+          <Grid item xs={8}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '95%' }}>
+              <h1>All Contacts (100)</h1>
+              <Button style={{
+                backgroundColor: "#09A391", borderRadius: '35px', height: "40px",
+                minWidth: "10px", margin: 'auto 0 auto auto'
+              }} variant="contained">+</Button>
+            </div>
+            <TextField placeholder="Search Contacts" id="outlined-basic" variant="outlined" style={{ width: '95%', backgroundColor: '#F3F5F9', color: '#DEE3ED' }} />
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '95%' }}>
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <Button
+                  onClick={selectAllContacts}
+                  style={{
+                    backgroundColor: "#B3BFD2", borderRadius: '35px', height: "40px",
+                    minWidth: "10px", margin: 'auto'
+                  }} variant="contained"><AiOutlineCheck style={{ backgroundColor: "color" }} /></Button>
+                <h4 style={{ paddingLeft: "24%", margin: "0" }}>Select All</h4>
+              </div>
+              <div style={{ margin: "auto 0" }}>
+                <Button style={{
+                  backgroundColor: "#09A391", borderRadius: '10px', height: "40px",
+                  minWidth: "10px", textTransform: 'capitalize'
+                }} variant="contained">Export All</Button>
+              </div>
+            </div>
+            <div style={{ overflow: 'auto', height: '630px' }}>
+              <h3>A</h3>
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <h3>B</h3>
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <h3>C</h3>
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+              <Chats selectAll={selectAll} />
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+    </div >
+  );
+};
+
+export default Main;
